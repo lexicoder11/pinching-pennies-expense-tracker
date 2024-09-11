35f8cf5a4c46cfde7dc1a515df80a48b21a74485
Lexi Allen <lexi@Lexis-Mac-mini.home>
2024-09-10 18:46:59 -0600

    small updates

--- pinching-pennies/App.js
+++ pinching-pennies/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports'; // Ensure this path is correct
-import AppNavigator from './navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// Configure AWS Amplify
-Amplify.configure(awsconfig);
+import Welcome from './components/Welcome';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import ExpenseOverview from './components/ExpenseOverview';
+import ExpenseAdd from './components/ExpenseAdd';
+import ExpenseDelete from './components/ExpenseDelete';
+import ExpenseEdit from './components/ExpenseEdit';
 
-export default function App() {
-  return <AppNavigator />;
-}
+const Stack = createStackNavigator();
 
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="ExpenseOverview" component={ExpenseOverview} />
+      <Stack.Screen name="ExpenseAdd" component={ExpenseAdd} />
+      <Stack.Screen name="ExpenseDelete" component={ExpenseDelete} />
+      <Stack.Screen name="ExpenseEdit" component={ExpenseEdit} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
 
 
